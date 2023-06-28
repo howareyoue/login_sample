374da9be4850759170e96056ec550addafd767f2
yunho <yunhoo0511@naver.com>
2023-06-28 16:41:47 +0900

    make the button(+settings)

--- app/src/main/java/com/example/login_sample/MainActivity.java
+++ app/src/main/java/com/example/login_sample/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.login_sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button koreabutton;
+    private Button chinaButton;
+    private Button japanbutton;
+    private Button usabutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button koreabutton = findViewById(R.id.korea);
+        koreabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,koreafoodActivity.class);
+            }
+        });
+        chinaButton = findViewById(R.id.china);
+        chinaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        japanbutton= findViewById(R.id.japan);
+        japanbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        usabutton=findViewById(R.id.usa);
+        usabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
